bc0645879b819782edc8e6351d534309976882f4
samhol <sami.holck@gmail.com>
2017-11-14 19:47:10 +0200

    updates

---
+++ sivut/kilpailut/kalastus/2017.php
@@ -0,0 +1,35 @@
+#KALASTUSKILPAILU <small>2.9.2017</small>
+
+##"Matosarja"
+ 
+1. Håkan Hermansson: 430 g
+2. Sirkka Arpalahti: 20 g
+
+##Heittouistinsarja (naiset)
+
+1. Bella Sorjasaari: 3880 g
+2. Raija Viljanen: 2410 g
+
+##Heittouistinsarja (miehet)
+
+1. Stefan Kaapro: 8300 g
+2. Kari Sorjasaari: 4200 g
+3. Veikko Viljanen: 1630 g
+4. Matti Arpalahti: 780 g
+
+##Lastensarja
+
+1. Valtteri Fagerström: 1270 g
+2. Oona Kemppainen ja Aada Virtanen: 680 g
+
+##Yhteenveto
+
+Kiertopalkinnon voittaja: **Stefan Kaapro**
+
+Koko kilpailun yhteenlaskettu saalismäärä oli 23 kg 600 g 
+
+Suurin kala **Kari Sorjasaari** 2430 g
+
+
+<?php $kalastusKilpailut(); ?>
+
